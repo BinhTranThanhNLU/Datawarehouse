cd3d49caba01f838e469a3d53995aed61d9626d8
Hai-Hay-Ho <hominhhai2k4@gmail.com>
2025-11-20 12:22:12 +0700

    Tạo skeleton

---
+++ etl/aggregate.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from database import warehouse_engine, presentation_engine
+from .logger import log_start, log_end
+
+def run_aggregate():
+    pass
+
+if __name__ == "__main__":
+    run_aggregate()
